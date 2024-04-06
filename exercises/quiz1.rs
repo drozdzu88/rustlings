e38fbd1d0c5adf66dd6e6e2c09b84ae5a74ff95b
drozdzu88 <drozdzu88@gmail.com>
2024-04-06 20:38:04 +0200

    first commit

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,28 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+use std::io;
+
+// fn calculate_price_of_apples(number: u32) -> u32 {
+//     if number <= 40 {
+//         number * 2
+//     } else {
+//         number *1
+//     }
+// }
+
+fn calculate_price_of_apples(number: u32) -> Result<u32, &'static str> {
+    if number > 0 {
+        if number <= 40 {
+            Ok(number * 2)
+        } else {
+            Ok(number * 1)
+        }
+    } else {
+        Err("Bad quantity")
+    }
+}
 
 // Don't modify this function!
 #[test]
@@ -25,9 +43,11 @@ fn verify_test() {
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
+    let price5 = calculate_price_of_apples(0);
 
-    assert_eq!(70, price1);
-    assert_eq!(80, price2);
-    assert_eq!(41, price3);
-    assert_eq!(65, price4);
+    assert_eq!(Ok(70), price1);
+    assert_eq!(Err("Bad quantity"), price5)
+    // assert_eq!(80, price2);
+    // assert_eq!(41, price3);
+    // assert_eq!(65, price4);
 }
